5d2590eaf2c88bb1b3474a66000b2109c65ebcb7
Valentin 'ThisIsMac' M <thisismac47@gmail.com>
2016-08-12 15:53:07 +0200

    hello world

---
+++ app.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2016 Keymetrics Team. All rights reserved.
+ * Use of this source code is governed by a license that
+ * can be found in the LICENSE file.
+ */
+
+var pmx         = require('pmx');
+var Probe       = pmx.probe();
+var Worker      = require('./lib/worker');
+
+pmx.initModule({
+
+  pid              : pmx.resolvePidPaths(['/var/run/elasticsearch/elasticsearch.pid',
+                                          '/var/run/elasticsearch.pid']),
+  widget : {
+    type             : 'generic',
+    logo             : 'https://www.elastic.co/static/img/logo-elastic.png',
+
+    // 0 = main element
+    // 1 = secondary
+    // 2 = main border
+    // 3 = secondary border
+    theme            : ['#39bdb1', '#1B2228', 'white', '#807C7C'],
+
+    el : {
+      probes  : true,
+      actions : true
+    },
+
+    block : {
+      issues  : true,
+      meta : true,
+      main_probes : ['Elastic status', 'Nodes', 'Shards', 'Indices', 'Documents', 'Store size']
+    }
+  }
+}, function(err, conf) {
+
+  var WORKER_INTERVAL     = (conf.workerInterval * 1000) || 2000;
+  var TARGET_URLS         = conf.urls || process.env.PM_HEALTHCHECK_URLS;
+
+  // register all workers
+  var worker = new Worker(WORKER_INTERVAL, TARGET_URLS);
+
+  // init all probes
+  worker.init();
+  worker.update();
+
+  // start all workers
+  setInterval(worker.update.bind(worker), WORKER_INTERVAL);
+});
